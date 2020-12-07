5b72323badc0597435d8d07571b89428e07e7e8b
Jingsong Tan <jasontanjingsong@gmail.com>
2020-12-06 19:04:02 -0600

    packege lock update

--- frontend/src/app/plot-comp/plot-comp.component.ts
+++ frontend/src/app/plot-comp/plot-comp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plot-comp',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plot-comp.component.scss']
 })
 export class PlotCompComponent implements OnInit {
+  // @Input() 
 
   public graph = {
     data: [
